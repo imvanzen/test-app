1fa88defce48316c7e475d6d340187b124e2da29
Jakub Reczko <jakub.r@masterborn.com>
2024-10-08 19:48:30 +0200

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+
+// Pobieranie hosta i portu ze zmiennych środowiskowych lub ustawienie wartości domyślnych
+const hostname = process.env.HOSTNAME || '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello World</title>
+      </head>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  `);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
